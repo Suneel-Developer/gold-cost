5ce8f342798d25700404883da8de87506d380f56
Suneel developer <suneeldeveloper11@gmail.com>
2023-08-30 17:51:56 +0500

    website

--- components/MobileNavigation/page.js
+++ components/MobileNavigation/page.js
@@ -1,12 +1,21 @@
 "use client"
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RiHomeFill } from "react-icons/ri";
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { AiOutlineSetting } from "react-icons/ai";
 import { HiOutlineUser } from "react-icons/hi";
+import { usePathname  } from 'next/navigation'
+
 import Link from 'next/link'
 
 const MobileNavigation = () => {
+  const router = usePathname ();
+  const [active, setActive] = useState("")
+
+  const currentPath = router;
+useEffect(() => {
+  setActive(currentPath)
+}, [])
 
   const Menus = [
     { icon: <RiHomeFill />, href: "/dashboard" },
@@ -15,18 +24,19 @@ const MobileNavigation = () => {
     { icon: <HiOutlineUser />, href: "/profile" },
   ]
 
-  const [active, setActive] = useState(0)
+  
+  
 
 
   return (
     <div className=' bg-mobile_color  max-h-[70px] w-full fixed bottom-0 left-0 right-0 z-10 '>
       <ul className=' flex justify-around relative  '>
-
+      {/* <p>Post: {router.query.slug}</p> */}
         {
           Menus.map((menu, i) => (
             <li key={i} className=' p-6 relative'>
-              <Link href={menu.href} className=' flex text-center ' onClick={() =>  setActive(i)}>
-                <span className={`text-white text-center text-2xl cursor-pointer   ${i === active && '-mt-[3.4rem] text-white bg-mobile_color border-4 flex justify-center items-center h-14 w-14 border-white text-xl rounded-full shadow-[0px 2px 10px 0px rgba(255, 255, 255, 0.25) inset]'}`}>
+              <Link href={menu.href} className='flex text-center' onClick={() =>  setActive(i)}>
+                <span className={`text-white text-center text-2xl cursor-pointer   ${menu.href === active && '-mt-[3.4rem] text-white bg-mobile_color border-4 flex justify-center items-center h-14 w-14 border-white text-xl rounded-full shadow-[0px 2px 10px 0px rgba(255, 255, 255, 0.25) inset]'}`}>
                   {menu.icon}
                 </span>
               </Link>
--- components/sidebar/page.js
+++ components/sidebar/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -7,7 +7,7 @@ import { BiSolidChevronsLeft } from "react-icons/bi";
 import { RiHomeFill } from "react-icons/ri";
 import { MdOutlineNotificationsActive } from "react-icons/md";
 import { HiOutlineUser, HiChartPie } from "react-icons/hi";
-import { FaUsers,FaUsersCog  } from "react-icons/fa";
+import { FaUsers, FaUsersCog } from "react-icons/fa";
 import { AiOutlineSetting } from "react-icons/ai";
 
 const MyComponent = ({ isOpen, hideSidebar }) => {
@@ -20,7 +20,7 @@ const MyComponent = ({ isOpen, hideSidebar }) => {
     >
       <div className="flex flex-col items-center h-full ">
         <div className="flex items-center justify-between w-full mb-10 ">
-          <h1 className="text-logocolor text-xl md:text-3xl font-black font-kavoon">
+          <h1 className="text-logocolor text-2xl md:text-3xl font-black font-kavoon">
             Gold Coast
           </h1>
           <button
@@ -35,109 +35,201 @@ const MyComponent = ({ isOpen, hideSidebar }) => {
           <li>
             <Link
               href="/dashboard"
-              className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                pathname.includes("dashboard") && "bg-active_bg p-2 text-white"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("dashboard") && "gap-0 -ml-3"
               }`}
             >
-              <RiHomeFill className="w-7 h-7" />
-              <span className="text-lg pt-1 text-dark_text font-medium tracking-wide">
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("dashboard") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
+                }`}
+              >
+                <RiHomeFill className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("dashboard") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
                 Home
               </span>
             </Link>
           </li>
 
           {/* Notification  */}
+
           <li>
             <Link
               href="/notification"
-              className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                pathname.includes("notification") && "bg-active_bg p-2 text-white"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("/notification") && "gap-0 -ml-3"
               }`}
             >
-              <MdOutlineNotificationsActive className="w-7 h-7" />
-              <span className="text-lg pt-1 text-dark_text font-medium tracking-wide	">
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("/notification") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
+                }`}
+              >
+                <MdOutlineNotificationsActive className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("/notification") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
                 Notification
               </span>
             </Link>
           </li>
 
           {/* Profile  */}
+
           <li>
             <Link
               href="/profile"
-              className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                pathname.includes("profile") && "bg-active_bg p-2 text-white"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("/profile") && "gap-0 -ml-3"
               }`}
             >
-              <HiOutlineUser className="w-7 h-7" />
-              <span className="text-lg pt-1 text-dark_text font-medium tracking-wide	">
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("/profile") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
+                }`}
+              >
+                <HiOutlineUser className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("/profile") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
                 Profile
               </span>
             </Link>
           </li>
 
           {/* Members  */}
+
           <li>
             <Link
               href="/members"
-              className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                pathname.includes("members") && "bg-active_bg p-2 text-white"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("/members") && "gap-0 -ml-3"
               }`}
             >
-              <FaUsers className="w-7 h-7" />
-              <span className="text-lg pt-1 text-dark_text font-medium tracking-wide	">
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("/members") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
+                }`}
+              >
+                <FaUsers className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("/members") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
                 Members
               </span>
             </Link>
           </li>
 
           {/* View Contributions  */}
+
           <li>
             <Link
               href="/contributions"
-              className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                pathname.includes("contributions") && "bg-active_bg p-2 text-white"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("/contributions") && "gap-0 -ml-3"
               }`}
             >
-              <HiChartPie className="w-7 h-7" />
-              <span className="text-lg pt-1 text-dark_text font-medium tracking-wide	">
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("/contributions") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
+                }`}
+              >
+                <HiChartPie className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("/contributions") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
                 View Contributions
               </span>
             </Link>
           </li>
 
+
           {/* Community  */}
+
           <li>
             <Link
               href="/community"
-              className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                pathname.includes("community") && "bg-active_bg p-2 text-white"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("/community") && "gap-0 -ml-3"
               }`}
             >
-              <FaUsersCog className="w-7 h-7" />
-              <span className="text-lg pt-1 text-dark_text font-medium tracking-wide	">
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("/community") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
+                }`}
+              >
+                <FaUsersCog className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("/community") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
                 Community
               </span>
             </Link>
           </li>
+
         </ul>
 
         <div className="mt-auto w-full pb-5">
           <ul>
             {/* Settings  */}
+
             <li>
-              <Link
-                href="/settings"
-                className={`flex items-center gap-6 rounded duration-300 ease-in-out mb-5 ${
-                  pathname.includes("settings") && "bg-active_bg p-2 text-white"
+            <Link
+              href="/settings"
+              className={`flex items-center rounded duration-300 gap-6 ease-in-out mb-5 ${
+                pathname.includes("/settings") && "gap-0 -ml-3"
+              }`}
+            >
+              <span
+                className={`text-dark_text ${
+                  pathname.includes("/settings") &&
+                  " text-white bg-mobile_color flex justify-center w-[60px] px-3 h-[55px] items-center  rounded-full z-10"
                 }`}
               >
-                <AiOutlineSetting className="w-8 h-8" />
-                <span className="text-xl pt-1 text-dark_text font-medium tracking-wide	">
-                  Settings
-                </span>
-              </Link>
-            </li>
+                <AiOutlineSetting className="text-2xl" />
+              </span>
+              <span
+                className={`text-lg text-dark_text w-full font-medium tracking-wide ${
+                  pathname.includes("/settings") &&
+                  "bg-active_bg p-2 text-white -ml-8 pl-5 "
+                }`}
+              >
+                Settings
+              </span>
+            </Link>
+          </li>
           </ul>
         </div>
       </div>
